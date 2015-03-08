663b10b84fdaa22dc753de413eb35b5210ddfcc6
Christopher Millward <cmillward@millwardesque.com>
2015-03-07 20:12:41 -0500

    Initial commit.

---
+++ Assets/Scripts/Jaws.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class Jaws : MonoBehaviour {
+	public GameObject target;
+	Animator animator;
+	public float xSpeed = 1.0f;
+	public float ySpeed = 1.0f;
+	public bool facingRight = true;
+
+	// Use this for initialization
+	void Awake() {
+		animator = GetComponent<Animator> ();
+
+		if (target == null) {
+			Debug.LogError("Unable to start Jaws: Target isn't set.");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = (target.transform.position - transform.position);
+
+		if (direction.x > float.Epsilon && facingRight != true) {
+			animator.SetTrigger ("Swim Right");
+			facingRight = true;
+		} else if (direction.x < -float.Epsilon && facingRight == true) {
+			animator.SetTrigger("Swim Left");
+			facingRight = false;
+		}
+		
+		Vector3 movement = new Vector3 (direction.x * xSpeed, direction.y * ySpeed);
+		transform.position += movement * Time.deltaTime;
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Animator))]
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (BoxCollider2D))]
+public class Player : MonoBehaviour {
+	Animator animator;
+	bool facingRight = true;
+	public float xSpeed = 1f;
+	public float ySpeed = 1f;
+
+	// Use this for initialization
+	void Awake() {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xDir = Input.GetAxis ("Horizontal");
+		float yDir = Input.GetAxis ("Vertical");
+		float deadzone = 0.0001f;
+
+		if (xDir > deadzone && facingRight != true) {
+			animator.SetTrigger ("Swim Right");
+			facingRight = true;
+		} else if (xDir < -deadzone && facingRight == true) {
+			animator.SetTrigger("Swim Left");
+			facingRight = false;
+		}
+
+		Vector3 movement = new Vector3 (xDir * xSpeed, yDir * ySpeed);
+		transform.position += movement * Time.deltaTime;
+	}
+}
