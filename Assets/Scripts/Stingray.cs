75e390414e067ee32ad60e64560da0ad714c33f1
Christopher Millward <christopher.millward@gmail.com>
2015-03-09 22:37:42 -0400

    Added delay to stingray motion after getting hit.

--- Assets/Scripts/Stingray.cs
+++ Assets/Scripts/Stingray.cs
@@ -6,6 +6,8 @@ using System.Collections;
 public class Stingray : MonoBehaviour {
 	public int health = 3;
 	public float speed = 3.0f;
+	public float delayAfterHit = 0.2f;
+	float currentHitDelay = 0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +16,12 @@ public class Stingray : MonoBehaviour {
 
 	void Update() {
 		if (IsAlive ()) {
-			transform.position += new Vector3 (speed * Time.deltaTime, 0f);
+			if (currentHitDelay <= float.Epsilon) {
+				transform.position += new Vector3 (speed * Time.deltaTime, 0f);
+			}
+			else {
+				currentHitDelay -= Time.deltaTime;
+			}
 		}
 	}
 
@@ -24,6 +31,7 @@ public class Stingray : MonoBehaviour {
 		}
 		else if (col.tag == "Bullet") {
 			AddDamage(col.GetComponent<Bullet>().damage);
+			currentHitDelay = delayAfterHit;
 		}
 	}
 
